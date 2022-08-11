ca733672b1a7c58bd80e2a6b65e0c53ccf6baa0c
RusuKrist <rusukrist30@yandex.ru>
2022-08-12 03:50:01 +0500

    задание 6

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -2,11 +2,10 @@
 int Number = int.Parse(Console.ReadLine());
 int remain = Number % 2;
 if (remain == 0)
-{   Console.WriteLine(" Число " + Number + " является чётным?");
-    Console.WriteLine("Да, является  чётным!");
+{    Console.WriteLine(" Число " + Number + " является чётным?");
+    Console.WriteLine("Да, число " + Number + " является  чётным!");
 }
 else
-{
-    Console.WriteLine(" Число " + Number + " является чётным?");
+{    Console.WriteLine(" Число " + Number + " является чётным?");
     Console.WriteLine("Нет, число " + Number + " не является чётным!");
 }
