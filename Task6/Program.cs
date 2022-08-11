1b72daff2d1e8c53e1a65e5772aa8ae2fac61b5f
RusuKrist <rusukrist30@yandex.ru>
2022-08-12 03:36:00 +0500

    задание 6

---
+++ Task6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int Number = int.Parse(Console.ReadLine());
+int remain = Number % 2;
+if (remain == 0)
+{   Console.WriteLine(" Число " + Number + " является чётным?");
+    Console.WriteLine("Да, является  чётным!");
+}
+else
+{
+    Console.WriteLine(" Число " + Number + " является чётным?");
+    Console.WriteLine("Нет, число " + Number + " не является чётным!");
+}
