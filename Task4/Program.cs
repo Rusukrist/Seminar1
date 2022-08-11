cd3bb9833538b7ac4a74e149fd6959813728f403
RusuKrist <rusukrist30@yandex.ru>
2022-08-12 02:59:52 +0500

    задание 4

---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число: ");
+int NumberA = int.Parse(Console.ReadLine());
+Console.Write("Введите  второе число: ");
+int NumberB = int.Parse(Console.ReadLine());
+Console.Write("Введите  третье число: ");
+int NumberC = int.Parse(Console.ReadLine());
+int maxNumber = 0;
+if (NumberA > maxNumber) maxNumber = NumberA;
+if (NumberB > maxNumber) maxNumber = NumberB;
+if (NumberC > maxNumber) maxNumber = NumberC;
+Console.Write("максимальное из трех цифр" + maxNumber);
+Console.ReadKey();
