a14db02fe6727c892608599037aa2367986ad7d4
RusuKrist <rusukrist30@yandex.ru>
2022-08-12 00:24:39 +0500

    задание 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+int NumberA = int.Parse(Console.ReadLine());
+Console.Write("Введите  второе число: ");
+int NumberB = int.Parse(Console.ReadLine());
+if (NumberA > NumberB);
+    Console.WriteLine($"{NumberA}, min ");
+    Console.WriteLine($"{NumberB}, max ");
+Console.ReadKey();
+
